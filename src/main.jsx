624aca0d0324c6a138c056218fa89d8c25b603ed
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2024-02-20 10:44:26 -0400

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+// react = biblioteca
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+
+// sessao dinamica, div que vai ser o container,
+// chama o render e em baixo o conteudo do react
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
