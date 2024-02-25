edb15b484ab70c9c07e0c5b492493c399c4eeaa2
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2024-02-25 11:49:36 -0400

    components react

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import Perfil from "./components/Perfil";
+
 function App() {
-  return <h1>Teste</h1>;
+  const name = "Artur";
+
+  function functionReturn() {
+    return "This is a function nameReturn";
+  }
+
+  const person = {
+    name: "Sarah",
+  };
+
+  let itsMorning = true;
+
+  return (
+    <>
+      <Perfil />
+      <h1>Olá {name}</h1>
+      <h1>Olá {functionReturn()}</h1>
+      <h1>Olá {person.name}</h1>
+      <p>Mais um projetinho se inicia...</p>
+      <hr />
+      {itsMorning === true ? "Bom dia!" : "Boa tarde!"} <br />
+      {itsMorning ? "Bom dia!" : "Boa tarde!"} <br />
+      {itsMorning && "Bom dia!"} <br />
+    </>
+  );
 }
 
 export default App;
