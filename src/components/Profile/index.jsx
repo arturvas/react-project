5f72374cbc2d6a4fae85ce837936e36e4cf77e33
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2024-02-27 12:48:05 -0400

    feat: button to toggle visibility

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,12 +1,14 @@
+import React from "react";
 import "./profile.css";
 
-const Profile = ({ address, name }) => {
+const Profile = ({ address, name, toggleFormVisibility }) => {
   return (
     <div className="container">
       {/* JSON.stringfy = converter um objeto em string */}
       {/* {JSON.stringify(props)} */}
       <img className="profile-avatar" src={address} />
       <h3 className="profile-title">{name}</h3>
+      <button onClick={toggleFormVisibility}>Toggle form</button>
     </div>
   );
 };
