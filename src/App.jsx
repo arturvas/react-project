5e61abf2329508f233a20c74c69c6c9ea3a9e2a4
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2024-02-27 12:48:43 -0400

    feat: button to toggle visibility of Formulary

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
+import { useState } from "react";
 import Profile from "./components/Profile";
 import Formulary from "./components/Formulary";
 
 function App() {
+  const [formIsVisible, setFormIsVisible] = useState(true);
+
+  const toggleFormVisibility = () => {
+    setFormIsVisible(!formIsVisible);
+  };
+
   return (
     <>
       <Profile
         name="Artur Vasconcelos"
         address="https://github.com/arturvas.png"
+        toggleFormVisibility={toggleFormVisibility}
       />
-      <Formulary />
+      {/* <button onClick={() => setFormIsVisible(!formIsVisible)} type="button">
+        Toggle form
+      </button> */}
+      {formIsVisible && <Formulary />}
     </>
   );
 }
