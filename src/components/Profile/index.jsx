579d2dd32ca8915b77c200dfb6002a1a7563aa74
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2024-02-26 22:10:42 -0400

    feat: add image and title for Profile component

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,16 +1,12 @@
 import "./profile.css";
 
-const Profile = () => {
-  const user = {
-    name: "Artur Vasconcelos",
-    avatar: "https://github.com/arturvas.png",
-  };
-
+const Profile = ({ address, name }) => {
   return (
     <div className="container">
+      {/* JSON.stringfy = converter um objeto em string */}
       {/* {JSON.stringify(props)} */}
-      <img className="profile-avatar" src={user.avatar} />
-      <h3 className="profile-title">{user.name}</h3>
+      <img className="profile-avatar" src={address} />
+      <h3 className="profile-title">{name}</h3>
     </div>
   );
 };
