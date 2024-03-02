8c58f4a6363cdb56c3795841cc3fb159f0f41559
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2024-03-02 17:29:04 -0400

    feat: import `global.css`

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+import "./global.css";
+
 // sessao dinamica, div que vai ser o container,
 // chama o render e em baixo o conteudo do react
 ReactDOM.createRoot(document.getElementById("root")).render(
