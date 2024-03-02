8a46ae97ab89047db567d442819001d9f8190b55
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2024-03-02 16:31:38 -0400

    feat: add repository list

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Profile from "./components/Profile";
 import Formulary from "./components/Formulary";
+import ReposList from "./components/ReposList";
 
 function App() {
   const [formIsVisible, setFormIsVisible] = useState(true);
@@ -16,6 +17,7 @@ function App() {
         address="https://github.com/arturvas.png"
         toggleFormVisibility={toggleFormVisibility}
       />
+      <ReposList />
       {/* <button onClick={() => setFormIsVisible(!formIsVisible)} type="button">
         Toggle form
       </button> */}
