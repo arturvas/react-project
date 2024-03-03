42acfaee764010b53b9710966f9ed7a2d8514432
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2024-03-02 21:22:45 -0400

    feat: add Loading effect when name inserted

--- src/components/ReposList/index.jsx
+++ src/components/ReposList/index.jsx
@@ -2,40 +2,44 @@ import { useEffect, useState } from "react";
 
 import styles from "./ReposList.module.css";
 
-const ReposList = () => {
+const ReposList = ({ userName }) => {
   const [repos, setRepos] = useState([]);
   const [areLoading, setAreLoading] = useState(true);
 
   useEffect(() => {
-    fetch("https://api.github.com/users/arturvas/repos")
+    setAreLoading(true);
+    fetch(`https://api.github.com/users/${userName}/repos`)
       .then((res) => res.json())
       .then((resJson) => {
         setTimeout(() => {
           setAreLoading(false);
           setRepos(resJson);
-        }, 3000);
+        }, 2000);
       });
-  }, []);
+  }, [userName]);
 
   return (
     <>
       <div className="container">
-        {areLoading && <h1>Loading...</h1>}
-        <ul className={styles.list}>
-          {repos.map(({ id, name, language, html_url }) => (
-            <li className={styles.listItem} key={id}>
-              <div className={styles.itemName}>
-                <b>Name:</b> {name} <br />
-              </div>
-              <div className={styles.itemLanguage}>
-                <b>Language:</b> {language} <br />
-              </div>
-              <a className={styles.itemLink} target="_blank" href={html_url}>
-                Visit on GitHub
-              </a>
-            </li>
-          ))}
-        </ul>
+        {areLoading ? (
+          <h1 className={styles.textCenter}>Loading...</h1>
+        ) : (
+          <ul className={styles.list}>
+            {repos.map(({ id, name, language, html_url }) => (
+              <li className={styles.listItem} key={id}>
+                <div className={styles.itemName}>
+                  <b>Name:</b> {name} <br />
+                </div>
+                <div className={styles.itemLanguage}>
+                  <b>Language:</b> {language} <br />
+                </div>
+                <a className={styles.itemLink} target="_blank" href={html_url}>
+                  Visit on GitHub
+                </a>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </>
   );
